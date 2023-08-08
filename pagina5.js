e6598a567cd9f591afece5f7e4c3acd210167575
Sofia19c <sofivampaireknight@gmail.com>
2023-08-07 22:23:23 -0500

    medio funciona el de la tabla

--- pagina5.js
+++ pagina5.js
@@ -1,18 +1,39 @@
-(function(){
-var formulario = document.getElementsByName('formulario')[0], 
+var usuarios = [];
+
+(function () {
+  const botonDos = document.getElementById("agregarFila");
+  botonDos.addEventListener("click", agregarFila);
+
+  function agregarFila() {
+    const tablita = document
+      .getElementById("filas")
+    const nuevaFila = tablita.insertRow();
+
+    const Username = nuevaFila.insertCell(0);
+    const Nombre = nuevaFila.insertCell(1);
+    const Imagen = nuevaFila.insertCell(1);
+    const Mascotas = nuevaFila.insertCell(1);
+
+    Username.innerHTML = "Nuevo Nombre";
+    Nombre.innerHTML = "Nueva Edad";
+    Imagen.innerHTML = "Nueva Edad";
+    Mascotas.innerHTML = "Nueva Edad";
+  }
+  var formulario = document.getElementsByName("formulario")[0],
     elementos = formulario.elements,
-    boton= document.getElementById('btn');
+    boton = document.getElementById("btn");
 
-    var validarUsername = function(){
-        if(formulario.user.value == ""){
-            alert("Completa el campo Username");
-        }
-    };
+  var validarUsername = function () {
+    if (!formulario.user.value.includes(".")) {
+      alert("Porfavor asegurece de tener el punto");
+    }
+  };
+  var validar = function (e) {
+    e.preventDefault();
+    validarUsername();
+  };
 
-    var validar = function(e){
-        e.preventDefault();
-        validarUsername();
-    };
+  formulario.addEventListener("submit", validar);
+})();
 
-    formulario.addEventListener("submit", validar)
-}())
+document.addEventListener(DOMContentLoaded, function () {});
